f93a4765507d9bdc16e6164ad9f3e7f5a484dd5b
Samuel Suarez <samuelsuarez.lhs@gmail.com>
2022-08-30 18:59:12 -0500

    condicionales

--- main.js
+++ main.js
@@ -2,15 +2,15 @@
 
 alert("Saludos desde " + country + ", tenga una buena noche");
  */
-let numero = (Number.prompt("Escribe un número"));
-if(numero>1000){
-  alert("Es mayor que 1000")
+let numero = Number(prompt("Escribe un número"));
+if (numero > 1000) {
+  alert("Es mayor que 1000");
 }
-let text = prompt("Saludame")
-if(text="Hola"){
-  console.log("Hola")
+let text = prompt("Saludame");
+if ((text = "Hola")) {
+  console.log("Hola");
 }
-let numero2 = (Number.prompt("Escribe un numero"))
-if((numero2>=10)&&(numero2<=50)){
-  alert("Tu numero esta entre 10 y 50")
+let numero2 = Number(prompt("Escribe un numero"));
+if (numero2 >= 10 && numero2 <= 50) {
+  alert("Tu numero esta entre 10 y 50");
 }
